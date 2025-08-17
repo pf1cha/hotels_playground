63940175e65dd4c45f1c931f52d2a640c9bc2554
Max <lonishin.max@gmail.com>
2025-08-17 20:38:22 +0300

    some changes in the main file

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import pandas as pd
-from model import logistic_model_training, grid_search, svm_model
+from model import *
 from preprocessing import *
 from plot_results import *
+
+
+def print_results(info_about_results, confusion_matrix, hotels_label):
+    print(f"Precision: {info_about_results[0]}")
+    print(f"Recall: {info_about_results[1]}")
+    print(f"F1 Score: {info_about_results[2]}")
+    plot_confusion_matrix(confusion_matrix, hotels_label)
+
 
 if __name__ == "__main__":
     hotels = pd.read_csv('data/hotel_booking.csv')
     hotels_cleaned = cleaned_data(hotels)
-    # correlation_heatmap(hotels_cleaned)
     hotels_dummies = handling_categorical_features(hotels_cleaned)
-    hotels_scaled = scaling_features(hotels_dummies)
-    info_about_results, confusion_matrix = svm_model(hotels_scaled)
+    # hotels_scaled = scaling_features(hotels_dummies)
     hotels_label = ['Not Canceled', 'Canceled']
-    plot_confusion_matrix(confusion_matrix, hotels_label)
-    print(f"Precision: {info_about_results[0]}")
-    print(f"Recall: {info_about_results[1]}")
-    print(f"F1 Score: {info_about_results[2]}")
-
-    # grid_search(hotels_scaled)
+    feature_importance_df, info_about_results, confusion_matrix = feature_importance(hotels_dummies)
+    print_results(info_about_results, confusion_matrix, hotels_label)
+    plot_feature_importance(feature_importance_df)
